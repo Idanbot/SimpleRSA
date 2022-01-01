8ee25e08177ce965c9e169ed547c1e26939caf49
Idan Botbol <82098495+Idanbot@users.noreply.github.com>
2022-01-02 00:12:23 +0200

    init commit

---
+++ RSA/src/rsa/RSA.java
@@ -0,0 +1,72 @@
+package rsa;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+//	In case you want to create a list of possible keys
+//import java.util.ArrayList;
+//import java.util.List;
+
+public class RSA {
+
+	public static void main(String[] args) {
+
+		// Generating primes with given bit_length (primeSize)
+		int primeSize = 6;
+		SecureRandom r1 = new SecureRandom();
+		BigInteger bigP = BigInteger.probablePrime(primeSize, r1);
+
+		// Making sure the prime numbers selected are different from each other
+		BigInteger bigQ = BigInteger.probablePrime(primeSize, r1);
+		while (bigQ.compareTo(bigP) == 0)
+			bigQ = BigInteger.probablePrime(primeSize, r1);
+
+		// Printing generated P and Q
+		System.out.println("Prime P = " + bigP);
+		System.out.println("Prime Q = " + bigQ);
+
+		// Calculating products of primes
+		BigInteger productN = bigP.multiply(bigQ);
+		BigInteger phi = bigP.subtract(BigInteger.ONE).multiply(bigQ.subtract(BigInteger.ONE));
+
+		
+		
+		// Generating possible keys into a list
+//		List<BigInteger> possibleKeys = new ArrayList<>();
+//		for (BigInteger i = BigInteger.TWO; i.compareTo(phi) == -1; i = i.add(BigInteger.ONE)) {
+//			if (productN.gcd(i).compareTo(BigInteger.ONE) == 0 && phi.gcd(i).compareTo(BigInteger.ONE) == 0) {
+//				possibleKeys.add(i);
+//			}
+//		}
+//		
+//		// Printing generated possible key list
+//		System.out.println(possibleKeys);
+
+		
+		
+		
+		// Generating an encryption key
+		BigInteger eKey;
+		do
+			eKey = new BigInteger(phi.bitLength(), r1);
+		while (eKey.compareTo(BigInteger.ONE) <= 0 || eKey.compareTo(phi) >= 0 || !eKey.gcd(phi).equals(BigInteger.ONE));
+		System.out.println("Encryption key = " + eKey);
+
+		// Generating a decryption key
+		BigInteger dKey = eKey.modInverse(phi);
+		System.out.println("Decryption key = " + dKey);
+
+		
+		// Message encryption
+		BigInteger message = BigInteger.valueOf(1337);
+		BigInteger msgEn = message.modPow(eKey, productN);
+		System.out.println("Message: " + message);
+		System.out.println("Encrypted Message => " + msgEn);
+
+		// Message decryption
+		BigInteger msgDe = msgEn.modPow(dKey, productN);
+		System.out.println("Decryped Messaged => " + msgDe);
+
+	}
+
+}
